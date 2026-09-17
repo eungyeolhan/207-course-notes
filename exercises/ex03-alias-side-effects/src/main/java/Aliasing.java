169532e065ec4a8cebd1076d08f5635ce9495dd3
eungyeolhan <eungyeolhan404@gmail.com>
2026-09-16 21:14:24 -0400

    finished exercise 3

--- exercises/ex03-alias-side-effects/src/main/java/Aliasing.java
+++ exercises/ex03-alias-side-effects/src/main/java/Aliasing.java
@@ -40,6 +40,9 @@ public class Aliasing {
      */
     public static void addInPlace(int[] arr, int amount) {
         // TODO: complete
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] += amount;
+        }
     }
 
     /**
@@ -52,6 +55,11 @@ public class Aliasing {
      */
     public static int[] addCopy(int[] arr, int amount) {
         // TODO: complete
-        return new int[1];
+        int[] copy;
+        copy = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            copy[i] = arr[i] + amount;
+        }
+        return copy;
     }
 }
